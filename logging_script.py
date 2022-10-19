b3d6821969d0adc93f5386627070a1cb7b7fcb36
Craig <craigpinto17@gmail.com>
2022-10-19 18:20:17 -0400

    Create logging_script.py

---
+++ logging_script.py
@@ -0,0 +1,29 @@
+import os
+import logging
+from datetime import datetime
+
+DIRECTORY = os.getcwd()
+
+
+def start_logging(log_directory='') -> None:
+    """Standardized logging system starter
+    Call in every file for logging system to write to same file
+
+    Args:
+        log_directory: (str) Custom log path
+
+    Returns: None
+    """
+    log_filename = f"log-{datetime.today().strftime('%Y-%m-%d-%H')}.log"
+    if log_directory:
+        log_path = f"{log_directory}/{log_filename}"
+    else:
+        log_path = f"{DIRECTORY}/logs/{log_filename}"
+
+    root_logger = logging.getLogger()
+    root_logger.setLevel(logging.DEBUG)
+
+    handler = logging.FileHandler(log_path, 'a', 'utf-8')
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    handler.setFormatter(formatter)
+    root_logger.addHandler(handler)
